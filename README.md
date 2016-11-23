fce39bbc674194aee3abe73271f5967f82005d23
Aaron Shafovaloff <aaronshaf@gmail.com>
2016-11-22 18:12:52 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# babel-html-element
-HTMLElement replacement for using Babel to make custom elements
+You will need something like this for transpiling custom elements to ES5.
+
+## Relevant issues
+
+* https://github.com/babel/babel/issues/4480
+* https://github.com/w3c/webcomponents/issues/587#issuecomment-254126763
---
+++ index.js
@@ -0,0 +1,20 @@
+var __HTMLElement = HTMLElement
+if (window.customElements) {
+  __HTMLElement = BabelHTMLElement
+}
+
+// https://github.com/babel/babel/issues/4480
+var reConstruct = typeof Reflect === 'object' ?
+  Reflect.construct : function (Parent, args, Child) {
+    return Parent.apply(this, args)
+  }
+
+// https://github.com/w3c/webcomponents/issues/587#issuecomment-254126763
+function BabelHTMLElement() {
+  var newTarget = this.__proto__.constructor
+  return reConstruct(HTMLElement, [], newTarget)
+}
+Object.setPrototypeOf(BabelHTMLElement, HTMLElement)
+Object.setPrototypeOf(BabelHTMLElement.prototype, HTMLElement.prototype)
+
+module.exports = __HTMLElement
